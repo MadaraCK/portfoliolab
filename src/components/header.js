3835da96b4b961c15e1c3f2522c22457f4631501
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-06-07 16:29:02 +0200

    settings

---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../scss/main.scss'
+
+function Header() {
+    return (
+        <header className="header_layout">
+           <section className="img_header"></section>
+            <section className="main_section">
+                <p>XDXDXDD</p>
+            </section>
+        </header>
+    );
+}
+
+export default Header;
