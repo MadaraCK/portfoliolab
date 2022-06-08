b2c0e81e2799087d23c9b3b1ab18c3e3b4234557
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-06-08 15:59:10 +0200

    login

--- src/components/login.js
+++ src/components/login.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import '../scss/main.scss'
+import {Link} from "react-router-dom";
 
-function Login(props) {
+function Login() {
     return (
-        <div>zaloguj</div>
+        <div className="Login">
+            <section className="menu clearfix">
+                <div className="reg_login">
+                    <p>Zaloguj</p>
+                    <Link className="link_login" to="/newAccount">Załóż konto</Link>
+                </div>
+                <div className="menu_login">
+                    <ul className="ul_login_nav">
+                        <li className="login li_login_nav">
+                            <Link className="roll_login" to="/" spy={true} smooth={true} offset={50} duration={500} >Start</Link>
+                        </li>
+                        <li className="li_login_nav">
+                            <Link className="roll_login" to="/" spy={true} smooth={true} offset={50} duration={500} >O co chodzi?</Link>
+                        </li>
+                        <li className="li_login_nav">
+                            <Link  className="roll_login" to="/" spy={true} smooth={true} offset={50} duration={500} >O nas</Link>
+                        </li>
+                        <li className="li_login_nav">
+                            <Link className="roll_login" to="/" spy={true} smooth={true} offset={50} duration={500} >Fundacja i organizacje</Link>
+                        </li>
+                        <li className="li_login_nav">
+                            <Link className="roll_login" to="/" spy={true} smooth={true} offset={50} duration={500} >Kontakt</Link>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </div>
     );
 }
 
