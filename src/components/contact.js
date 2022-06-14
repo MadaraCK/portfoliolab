5ca061af4be7b8b3e4be51c81bc5e65fe65662a2
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-06-15 01:36:44 +0200

    background

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,9 +4,24 @@ function Contact() {
     return (
         <section className="contact_section">
             <div className="contact">
-                <h1 className="contact_main">Skontaktuj się z nami</h1>
+                <h1 className="contact_main">
+                    Skontaktuj się z nami
+                </h1>
                 <div className="contact_box">
-
+                    <div className="contact_input">
+                        <div className="name_input">
+                            <p>
+                                Wpisz swoje imię
+                            </p>
+                            <input type="text" placeholder="Krzysztof"/>
+                        </div>
+                        <div className="email_input">
+                            <p>
+                                Wpisz swój email
+                            </p>
+                            <input type="email" placeholder="abc@xyz.pl"/>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
