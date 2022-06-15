932ffbd33ec1c54ff9abefa52d24b880b141425e
Mikołaj Radek <radek_mikolaj@o2.pl>
2022-06-15 16:33:45 +0200

    contact

--- src/components/contact.js
+++ src/components/contact.js
@@ -7,20 +7,44 @@ function Contact() {
                 <h1 className="contact_main">
                     Skontaktuj się z nami
                 </h1>
-                <div className="contact_box">
-                    <div className="contact_input">
-                        <div className="name_input">
-                            <p>
+                <div
+                    className="contact_box">
+                    <div
+                        className="contact_input">
+                        <div
+                            className="name_input">
+                            <p
+                                className="text_input">
                                 Wpisz swoje imię
                             </p>
-                            <input type="text" placeholder="Krzysztof"/>
+                            <input
+                                type="text"
+                                placeholder="Krzysztof"
+                                className="input_text"
+                            />
                         </div>
-                        <div className="email_input">
-                            <p>
+                        <div
+                            className="email_input">
+                            <p
+                                className="text_input">
                                 Wpisz swój email
                             </p>
-                            <input type="email" placeholder="abc@xyz.pl"/>
+                            <input
+                                type="email"
+                                placeholder="abc@xyz.pl"
+                                className="input_text"
+                            />
                         </div>
+                    </div>
+                    <div className="textarea_box">
+                        <p>wpisz swoja wiadomosc</p>
+                        <textarea
+                            name="x"
+                            id=""
+                            cols="70"
+                            rows="6"
+                            placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.">
+                    </textarea>
                     </div>
                 </div>
             </div>
